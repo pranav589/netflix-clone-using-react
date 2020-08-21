f032ba36863d0b13d3dde3c8e22d7357cb7f10ca
pranav589 <pranav.bakale@gmail.com>
2020-08-21 21:53:36 +0530

    1 file modified, 1 added

--- src/Request.js
+++ src/Request.js
@@ -1,1 +1,14 @@
 const API_KEY="54c5f9878d1d0098141bbda3fb5ca441"
+
+const Request={
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals:`discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated:`movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies:`/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies:`/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorroMovies:`/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies:`/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries:`/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default Request
