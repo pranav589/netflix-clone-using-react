0862738ae87b8645bac8c0d402476f183c10e024
pranav589 <pranav.bakale@gmail.com>
2020-08-23 21:56:20 +0530

    3 files modified

--- src/Row.js
+++ src/Row.js
@@ -1,14 +1,17 @@
 import React,{useState,useEffect} from 'react'
 import axios from './axios'
 import './row.css'
+import Youtube from "react-youtube"
+import movieTrailer from 'movie-trailer'
 
 const base_url="https://image.tmdb.org/t/p/original/"
 
 function Row({title,fetchUrl,isLargeRow}){
   const [movies,setMovies]=useState([])
+  const [trailerUrl,setTrailerUrl]=useState('')
 
   useEffect(()=>{
-      //here the function that runs when Row.js loads
+      //here the function that kruns when Row.js loads
       async function fetchData(){
         const request=await axios.get(fetchUrl)
         console.log(request.data.results)
@@ -19,6 +22,27 @@ function Row({title,fetchUrl,isLargeRow}){
   },[fetchUrl])
    {/*fetchUrl is included in the useEffect from outside of the current component,hence it has to be included in the dependancy*/}
 console.log(movies)
+
+const opts = {
+      height: '390',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+      },
+    };
+
+  const handleClick=(movie)=>{
+    if(trailerUrl){
+      setTrailerUrl('')
+    }else{
+      movieTrailer(movie && movie.name || "").then(url=>{
+        const urlParams=new URLSearchParams(new URL(url).search)
+        urlParams.get('v')
+      }).catch(err=>console.log(err.message))
+    }
+  }
+ 
   return(
     <div className="row">
     {/*titles */}
@@ -27,9 +51,10 @@ console.log(movies)
       {/*container for posters */}
       <div className="row_posters">
         {movies.map(movie=>(
-          <img src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}  className={`row_poster ${isLargeRow && "row_posterLarge"}`} key={movie.id}/>
+          <img src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name}  className={`row_poster ${isLargeRow && "row_posterLarge"}`} key={movie.id} onClick={handleClick(movie)}/>
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>}
     </div>
   )
 }
