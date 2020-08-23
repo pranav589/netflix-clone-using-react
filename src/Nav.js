f548b5dc08f82f7c849f017bc3a1934c53c50c1f
pranav589 <pranav.bakale@gmail.com>
2020-08-23 20:22:47 +0530

    5 files modified

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import './Nav.css'
 
 function Nav(){
   return(
-    <div>
+    <div className='nav'>
        <img src="https://stackblitz.com/files/netflix-clone-using-react/github/pranav589/netflix-clone-using-react/master/src/580b57fcd9996e24bc43c529.png" className="nav_logo" alt="Netflix Logo"/>
 
        <img src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg" className="nav_avatar" alt="Netflix Avatar"/>
